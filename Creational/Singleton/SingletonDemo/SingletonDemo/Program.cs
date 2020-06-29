1f4812bb862d6b686d3e9835e35859840dda3b6d
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-06-29 17:38:19 +0200

    Singleton design pattern demo

---
+++ Creational/Singleton/SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.Instance;
+            s1.printMessage("Hello");
+
+            Singleton s2 = Singleton.Instance;
+            s2.printMessage("Singleton");
+
+        }
+    }
+}
---
+++ Creational/Singleton/SingletonDemo/SingletonDemo/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+
+        private static Singleton _instance;
+        private static int _instanceCounter { get; set; }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Singleton();
+                return _instance;
+            }
+        }
+
+        private Singleton()
+        {
+            Console.WriteLine("Number of instantiated objects: "+(++_instanceCounter));
+        }
+        
+        public void printMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+        
+    }
+}
